23f8f0a279f65498d533d71ed212b0b7f278824d
Diego Sabillon <117101226+DiegoS2004@users.noreply.github.com>
2025-06-06 00:06:01 -0600

    cambios a login

--- coinpilot/hooks/use-auth.tsx
+++ coinpilot/hooks/use-auth.tsx
@@ -79,7 +79,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: "google",
       options: {
-        redirectTo: `${window.location.origin}/dashboard`,
+        redirectTo: "https://coin-pilot.vercel.app/dashboard",
       },
     })
     return { data, error }
@@ -89,7 +89,7 @@ export function AuthProvider({ children }: { children: React.ReactNode }) {
     const { data, error } = await supabase.auth.signInWithOAuth({
       provider: "github",
       options: {
-        redirectTo: `${window.location.origin}/dashboard`,
+        redirectTo: "https://coin-pilot.vercel.app/dashboard",
       },
     })
     return { data, error }
